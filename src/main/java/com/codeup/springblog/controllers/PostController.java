cf7e2550de7c757c14bda2b8eac9021385e986c1
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-07-28 23:52:20 -0500

    completed walkthrough on exercise

--- src/main/java/com/codeup/springblog/controllers/PostController.java
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -3,20 +3,24 @@ package com.codeup.springblog.controllers;
 
 import com.codeup.springblog.models.Post;
 import com.codeup.springblog.models.PostRepository;
+import com.codeup.springblog.models.User;
+import com.codeup.springblog.models.UserRepository;
+import com.codeup.springblog.services.EmailService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Controller
 public class PostController {
     private final PostRepository postDao;
+    private final UserRepository userDao;
+    private final EmailService emailService;
 
 
-    public PostController(PostRepository postDao) {
+    public PostController(PostRepository postDao, UserRepository userDao, EmailService emailService) {
         this.postDao = postDao;
+        this.userDao = userDao;
+        this.emailService = emailService;
     }
 
     @GetMapping("/posts")
@@ -59,6 +63,22 @@ public class PostController {
     @ResponseBody
     public String createPost() {
         return "<h1>This creates a new post!</h1>";
+    }
+
+    @GetMapping("/posts/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@ModelAttribute Post post) {
+        User user = userDao.getById(1L);
+        post.setUser(user);
+        postDao.save(post);
+//        emailService.prepareAndSend(post, "You created: " + post.getTitle(), post.getBody());
+        emailService.prepareAndSend(post.toString(), "You created: " + post.getTitle(), post.getBody());
+        return "redirect:/posts";
     }
 
 }
