6afcefdb63b70d7785b18c73c64a7b54ffc58a41
Benny Alvarez <benjamin.c.alvarez.21@gmail.com>
2021-08-20 02:56:04 -0500

    practice more spring boot

--- src/main/java/com/codeup/springblog/controllers/PostsController.java
+++ src/main/java/com/codeup/springblog/controllers/PostsController.java
@@ -1,4 +1,22 @@
 package com.codeup.springblog.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
 public class PostsController {
+
+
+
+    @GetMapping("/the/posts")
+    @ResponseBody
+    public String thePosts() {
+
+        return "There will be posts here.";
+    }
+
+
+
 }
